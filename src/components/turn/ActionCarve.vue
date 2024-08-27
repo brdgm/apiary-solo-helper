b7bdd5b4ea24057e50517bf9badf1a9e2655d184
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-08-27 17:20:51 +0200

    carve

--- src/components/turn/ActionCarve.vue
+++ src/components/turn/ActionCarve.vue
@@ -1,10 +1,25 @@
 <template>
   <p>
     <WorkerIcon :worker="navigationState.worker" :value="4"/>?
-    <button class="btn btn-primary mt-1" @click="gainCarving()">
-      Carve {{navigationState.carveSteps}}
-    </button>
+    <a class="btn btn-primary mt-1" data-bs-toggle="modal" data-bs-target="#carveModal">
+      {{t('turnBot.carve.title', {value:navigationState.carveSteps})}}
+    </a>
   </p>
+
+  <ModalDialog id="carveModal" :title="t('turnBot.carve.title', {value:navigationState.carveSteps})">
+    <template #body>
+      <p class="mb-3" v-html="t('turnBot.carve.followPath', {position:t(`position.${navigationState.carveSteps}`)})"></p>
+      <AppIcon type="carve-s-curve" :name="navigationState.sCurve" class="s-curve"/>
+      <p class="mt-3" v-html="t('turnBot.carve.noTile')"></p>
+    </template>
+    <template #footer>
+      <button class="btn btn-primary" data-bs-dismiss="modal" @click="gainCarving()">
+        {{t('turnBot.carve.get')}}
+        <TileIcon :tile="carvingTile"/>
+      </button>
+      <button class="btn btn-secondary" data-bs-dismiss="modal">{{t('action.cancel')}}</button>
+    </template>
+  </ModalDialog>  
 </template>
 
 <script lang="ts">
@@ -13,11 +28,17 @@ import { useI18n } from 'vue-i18n'
 import NavigationState from '@/util/NavigationState'
 import WorkerIcon from '@/components/structure/WorkerIcon.vue'
 import Tile from '@/services/enum/Tile'
+import ModalDialog from '@brdgm/brdgm-commons/src/components/structure/ModalDialog.vue'
+import AppIcon from '../structure/AppIcon.vue'
+import TileIcon from '../structure/TileIcon.vue'
 
 export default defineComponent({
   name: 'ActionCarve',
   components: {
-    WorkerIcon
+    WorkerIcon,
+    ModalDialog,
+    AppIcon,
+    TileIcon
   },
   emits: {
     collectTiles: (_tiles: Tile[]) => true  // eslint-disable-line @typescript-eslint/no-unused-vars
@@ -32,6 +53,11 @@ export default defineComponent({
       required: true
     }
   },
+  computed: {
+    carvingTile(): Tile {
+      return Tile.CARVING
+    }
+  },
   methods: {
     gainCarving() : void {
       this.$emit('collectTiles', [Tile.CARVING])
@@ -39,3 +65,9 @@ export default defineComponent({
   }
 })
 </script>
+
+<style lang="scss" scoped>
+.s-curve {
+  width: 200px;
+}
+</style>
